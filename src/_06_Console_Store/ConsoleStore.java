1acaeb663ddad5b491ed284d9792ca6c98a148f8
league <league@imac-29.attlocal.net>
2024-09-07 16:55:54 -0700

    working on consol store step 3

--- src/_06_Console_Store/ConsoleStore.java
+++ src/_06_Console_Store/ConsoleStore.java
@@ -6,61 +6,63 @@ import _02_Generics_Store.*;
 
 public class ConsoleStore {
 
-    /*
-     * Write a program that simulates shopping in a store using the Scanner and
-     * the classes in Generics_Store.
-     * 
-     * Note: You may need to modify existing code in Generics Store and/or add
-     * additional items and pictures to fulfill all the requirements. You are
-     * also free to add any additional methods or classes in Console Store that
-     * might be helpful to you.
-     * 
-     * Requirements:
-     * 
-     * -Use Ternary operators in place of simple if/else statements and do-while
-     * loops instead of while loops where appropriate.
-     * 
-     * - There should be at least four unique items the user can buy. These can
-     * be food items, nonfood items or both.
-     * 
-     * - The user should have a stipend of money to spend and each item should
-     * have its own price.
-     * 
-     * -The user should have the ability to add items to their cart, remove
-     * items, view items or check out.
-     * 
-     * -The program should continue until the user chooses to check out.
-     * 
-     * -When the user checks out you should let them know if they do not have
-     * enough money to purchase all their items and offer to put items back.
-     * 
-     * -If the user successfully purchases the items you should remove the
-     * amount from their stipend, show them the pictures of what they bought and
-     * print out a receipt showing their name, the individual prices of the
-     * items and their total.
-     */
+	/*
+	 * Write a program that simulates shopping in a store using the Scanner and the
+	 * classes in Generics_Store.
+	 * 
+	 * Note: You may need to modify existing code in Generics Store and/or add
+	 * additional items and pictures to fulfill all the requirements. You are also
+	 * free to add any additional methods or classes in Console Store that might be
+	 * helpful to you.
+	 * 
+	 * Requirements:
+	 * 
+	 * -Use Ternary operators in place of simple if/else statements and do-while
+	 * loops instead of while loops where appropriate.
+	 * 
+	 * - There should be at least four unique items the user can buy. These can be
+	 * food items, nonfood items or both.
+	 * 
+	 * - The user should have a stipend of money to spend and each item should have
+	 * its own price.
+	 * 
+	 * -The user should have the ability to add items to their cart, remove items,
+	 * view items or check out.
+	 * 
+	 * -The program should continue until the user chooses to check out.
+	 * 
+	 * -When the user checks out you should let them know if they do not have enough
+	 * money to purchase all their items and offer to put items back.
+	 * 
+	 * -If the user successfully purchases the items you should remove the amount
+	 * from their stipend, show them the pictures of what they bought and print out
+	 * a receipt showing their name, the individual prices of the items and their
+	 * total.
+	 */
 
-    public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
-        Cart cart= new Cart();
-while(true) {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Cart cart = new Cart();
+		do {
 
-	System.out.println("1,Add || 2,Remove || 3,View || 4,Checkout");
-	int option= scanner.nextInt();
-	scanner.nextLine();
-	if(option==1) {
-		
+			System.out.println("1,Add || 2,Remove || 3,View || 4,Checkout");
+			int option = scanner.nextInt();
+			scanner.nextLine();
+			if (option == 1) {
+System.out.println("1,Candy || 2,Cereal || 3,Clothing || 4,Toy");
+int added=scanner.nextInt();
+scanner.nextLine();
+			}
+			if (option == 2) {
+
+			}
+			if (option == 3) {
+//do this next
+			}
+			if (option == 4) {
+
+			}
+		} while (true);
 	}
-if(option==2) {
-		
-	}
-if(option==3) {
-	
-}
-if(option==4) {
-	
-}
-}
-    }
 
 }
